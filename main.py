04694fe2a07815cd067468a3e7db5647b37783a4
Danil Konkov <buroroll@gmail.com>
2020-12-15 21:54:45 +0500

    commit

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 from par import *
 
-skills = []
-c = open('courses.txt', 'r')
-
 with open("skills.txt", encoding="utf-8") as file:
     skills = [row.strip() for row in file]
 
-for l in c.readlines():
-    par = Parser.get_program(l)
-    for s in skills:
-        if par.find(s) != -1:
-            print(l, s)
+parser = Parser()
+
+def openEdu_analysis():
+    c = open('openEDU.txt', 'r')
+    for l in c.readlines():
+        par = Parser().get_Openedu_program(l)
+        for line in par:
+            for s in skills:
+                if s.lower() in line.lower():
+                    print(l, s)
+
+
+def skillBox_analysis():
+    c = open('Skillbox.txt')
+    for l in c.readlines():
+        par = Parser().get_Skillbox_program(l)
+        for line in par:
+            for s in skills:
+                if s.lower() in line.lower():
+                    print(l, s)
+
+
+# openEdu_analysis()
+# skillBox_analysis()
+
+parser.get_Stepik_program('https://stepik.org/course/2223/promo')
--- par.py
+++ par.py
@@ -1,12 +1,62 @@
 import requests as req
+from bs4 import BeautifulSoup
+from selenium import webdriver
 
 
 class Parser(object):
+    driver = webdriver.Chrome()
+
     @staticmethod
-    def get_program(link):
+    def get_Openedu_program(link):
         head = {'user-agent': 'Chrome'}
         resp = req.get(link, headers=head)
         start = resp.text.rfind('Программа курса</h2>')
         end = resp.text.rfind('<h2 id="result_knowledge">')
         program = resp.text[start:end].replace('Программа курса</h2>', '')
         return program
+
+    @staticmethod
+    def get_Skillbox_program(link):
+        driver = webdriver.Chrome()
+        driver.get(link)
+        html = driver.page_source
+        driver.quit()
+        soup = BeautifulSoup(html, 'html.parser')
+        res = []
+        for elem in soup.find_all('button', class_='accordion__button js-accordion__button'):
+            for e in elem.find_all('span'):
+                res.append(e.text)
+        for elem in soup.find_all('button', class_='accordion__button js-accordion__button accordion__button--empty'):
+            for e in elem.find_all('span'):
+                res.append(e.text)
+        return res
+
+    @staticmethod
+    def get_Stepik_program(link):
+        driver = webdriver.Chrome()
+        driver.get(link)
+        html = driver.page_source
+        driver.quit()
+        soup = BeautifulSoup(html, 'html.parser')
+        res = []
+        for elem in soup.find_all('button', class_='accordion__button js-accordion__button'):
+            for e in elem.find_all('span'):
+                res.append(e.text)
+        for elem in soup.find_all('button', class_='accordion__button js-accordion__button accordion__button--empty'):
+            for e in elem.find_all('span'):
+                res.append(e.text)
+        return res
+
+    def get_Stepik_program(self, link):
+        self.driver.get(link)
+        self.driver.implicitly_wait(10)
+        e = self.driver.find_elements_by_xpath(
+            '//div[@class="toc-promo__section"]')
+        print(len(e))
+        for i in e:
+            i.click()
+        # elem.click()
+        # html = self.driver.page_source
+        # self.driver.quit()
+        # soup = BeautifulSoup(html, 'html.parser')
+        # print(soup)
